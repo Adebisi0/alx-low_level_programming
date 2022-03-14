2ef0a55ec98abf2c6e71eda6d58fb40b27db97a4
Adebisi <mercyadebisi25@gmail.com>
2022-03-14 06:40:46 -0700

    0x00-hello_world task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * printf task
+ * Return: 0 after printing function
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n")
+return (0);
+}
